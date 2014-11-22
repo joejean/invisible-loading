19dd7d011e2a56789501ecbcb57cda7158f799f2
mick <jj1192@nyu.edu>
2014-11-22 16:31:41 +0400

    add the rest of entitie

--- inviloader/inviloader.py
+++ inviloader/inviloader.py
@@ -10,6 +10,19 @@ cur.execute("CREATE TABLE organization(organizationID serial PRIMARY KEY, name v
 cur.execute("CREATE TABLE company(organizationID serial PRIMARY KEY REFERENCES organization(organizationID) );")
 cur.execute("CREATE TABLE continent(placeID serial PRIMARY KEY REFERENCES place(placeID));")
 cur.execute("CREATE TABLE country(placeID serial PRIMARY KEY REFERENCES place(placeID));")
+
+cur.execute("CREATE TABLE person(personID serial PRIMARY KEY, firstName varchar(25), lastName varchar(25), gender varchar(10), birthday date, email varchar(30), speaks varchar(100), browserUsed varchar(20), locationIP varchar(20), creationDate TIMESTAMP);")
+cur.execute("CREATE TABLE forum(forumID serial PRIMARY KEY, title varchar(20), creationDate TIMESTAMP);")
+cur.execute("CREATE TABLE post(postID serial PRIMARY KEY REFERENCES message(messageID), creationDate TIMESTAMP REFERENCES message(creationDate), browserUsed varchar(20) REFERENCES message(browserUsed),locationIP varchar(20) REFERENCES message(locationIP), content text, language varchar(15), imageFile varchar(30))";)
+
+cur.execute("CREATE TABLE tag(tagID serial PRIMARY KEY, name varchar(20));")
+cur.execute("CREATE TABLE tagclass(tagclassID serial PRIMARY KEY,\
+	name varchar(20)\
+	)";)
+cur.execute("CREATE TABLE university(universityID serial PRIMARY KEY REFERENCES organization(organizationID),\
+	name varchar(30) REFERENCES organization(name)\
+	);")
+
 conn.commit()
 cur.close()
 conn.close()
